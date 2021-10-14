5cb6003d6645792b0628090359cc234b920c4d28
Adi770 <alepa2155@gmail.com>
2021-10-14 20:58:11 +0200

    Fixed the problem with swagger and resource handler

--- Backend/crudapp/src/main/java/pl/lepa/crudapp/configuration/WebMvcConfiguration.java
+++ Backend/crudapp/src/main/java/pl/lepa/crudapp/configuration/WebMvcConfiguration.java
@@ -2,15 +2,28 @@ package pl.lepa.crudapp.configuration;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
-public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**")
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/swagger-ui/")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+                .addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry
+                .addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/images/");
     }
 }
